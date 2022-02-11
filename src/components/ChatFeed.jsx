7c7bdf688e4fb2ccbb7426776567ba7dc92e88d0
sakshamdani1402 <sakshamdsni@gmail.com>
2022-02-12 00:24:33 +0530

    resume form 22:13

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,5 +1,19 @@
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
 const ChatFeed = (props) =>{
-    console.log(props);
+    const {chats, activeChat, userName, messages} = props;
+    const chat = chats && chats[activeChat];
+
+    const  renderMessages = ()=>{
+        const keys = Object.keys(messages);
+        return keys.map((key,index) => {
+            const message = messages[key];
+            
+        })
+    }
+    renderMessages();
     return (
         <div>
             ChatFeed
